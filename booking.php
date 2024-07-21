c2ae2e289d25dc1a05e24aab1fc8800b97b3a2b1
muFathy-nuggettours <167089371+muFathy-nuggettours@users.noreply.github.com>
2024-07-21 13:35:32 +0300

    Update booking.php

--- booking.php
+++ booking.php
@@ -2,13 +2,13 @@
 
 //Validate Booking Session
 $booking_session = mysqlFetch(mysqlQuery("SELECT * FROM flights_reqeusts WHERE session='" . $get["id"] . "'"));
-if (!$booking_session){
+if (!$booking_session) {
 	brokenLink();
 }
 $itinerary = json_decode($booking_session["search_object"], true);
 
 //If itinerary currency is not equal to user currency
-if ($itinerary["currency"]!=$user_paymentCurrency["code"]){
+if ($itinerary["currency"] != $user_paymentCurrency["code"]) {
 	brokenLink();
 }
 
@@ -16,9 +16,9 @@ if ($itinerary["currency"]!=$user_paymentCurrency["code"]){
 //Step [1] - Register and assign travelers
 //========================================
 
-if ($booking_session["status"]==0 && $post["token"]){
+if ($booking_session["status"] == 0 && $post["token"]) {
 	//Create user account if not logged in
-	if ($post['email'] && !$logged_user){
+	if ($post['email'] && !$logged_user) {
 		$new_record_id = newRecordID("users_database");
 		$mobile_phone_code = getData("system_database_countries", "code", $post["country"], "phone_code");
 		$mobile_prefix = "+" . $mobile_phone_code;
@@ -31,19 +31,19 @@ if ($booking_session["status"]==0 && $post["token"]){
 
 		//Server side validation
 		$errors = array();
-		if (mysqlNum(mysqlQuery("SELECT * FROM users_database WHERE mobile='$mobile' OR mobile_conventional='$mobile_conventional'"))){
-			array_push($errors, readLanguage('accounts','mobile_registered') . " <a class=alert-link href='reset-password/'>" . readLanguage('accounts','reset_password') . "</a>");
+		if (mysqlNum(mysqlQuery("SELECT * FROM users_database WHERE mobile='$mobile' OR mobile_conventional='$mobile_conventional'"))) {
+			array_push($errors, readLanguage('accounts', 'mobile_registered') . " <a class=alert-link href='reset-password/'>" . readLanguage('accounts', 'reset_password') . "</a>");
 		}
-		if (mysqlNum(mysqlQuery("SELECT id FROM users_database WHERE email='$email'"))){
-			array_push($errors, readLanguage('accounts','email_registered') . " <a class=alert-link href='reset-password/'>" . readLanguage('accounts','reset_password') . "</a>");
+		if (mysqlNum(mysqlQuery("SELECT id FROM users_database WHERE email='$email'"))) {
+			array_push($errors, readLanguage('accounts', 'email_registered') . " <a class=alert-link href='reset-password/'>" . readLanguage('accounts', 'reset_password') . "</a>");
 		}
 		$rules["name"] = array("required", "max_length(100)");
 		$rules["email"] = array("required", "max_length(100)", "email");
 		$validation_result = SimpleValidator\Validator::validate($post, $rules);
 
-		if ($validation_result->isSuccess()==false){
-			$error = readLanguage('general','error');
-		} else if ($errors){
+		if ($validation_result->isSuccess() == false) {
+			$error = readLanguage('general', 'error');
+		} else if ($errors) {
 			$error = "<ul><li>" . implode("</li><li>", $errors) . "</li></ul>";
 		} else {
 			$query = "INSERT INTO users_database (
@@ -74,18 +74,18 @@ if ($booking_session["status"]==0 && $post["token"]){
 				'" . time() . "'
 			)";
 			mysqlQuery($query);
-			
+
 			userLogin($hash, true);
 			$logged_user = mysqlFetch(mysqlQuery("SELECT * FROM users_database WHERE hash='$hash'"));
 		}
 	}
 
 	//Insert passengers
-	if (!$error){
+	if (!$error) {
 		$passengers = array();
-		for ($i = 0; $i < intval($post['travelers']); $i++){
+		for ($i = 0; $i < intval($post['travelers']); $i++) {
 			$exists = mysqlFetch(mysqlQuery("SELECT id FROM users_passengers WHERE user_id='" . $logged_user["id"] . "' AND ssn='" . $post["ssn-$i"] . "'"));
-			if ($exists){
+			if ($exists) {
 				$query = "UPDATE users_passengers SET
 					type='" . $post["type-$i"] . "',
 					name_prefix='" . $post["name_prefix-$i"] . "',
@@ -102,7 +102,6 @@ if ($booking_session["status"]==0 && $post["token"]){
 				WHERE id=" . $exists["id"];
 				mysqlQuery($query);
 				array_push($passengers, $exists["id"]);
-				
 			} else {
 				$query = "INSERT INTO users_passengers (
 					type,
@@ -145,14 +144,16 @@ if ($booking_session["status"]==0 && $post["token"]){
 			notes='" . $post["notes"] . "',
 			status=1
 		WHERE id='" . $booking_session["id"] . "'");
-		
+
 		//Creat AirPrice Request
 		travelportAirPrice($booking_session["id"]);
 
 		$booking_session = mysqlFetch(mysqlQuery("SELECT * FROM flights_reqeusts WHERE id='" . $booking_session["id"] . "'"));
 	}
-	
-	if ($error){ $message = "<div class='alert alert-danger'>$error</div>"; }
+
+	if ($error) {
+		$message = "<div class='alert alert-danger'>$error</div>";
+	}
 }
 
 //========================================
@@ -161,9 +162,9 @@ if ($booking_session["status"]==0 && $post["token"]){
 
 $trip_currency = getData("system_payment_currencies", "code", $itinerary["currency"]);
 $trip_price = $itinerary["price"];
-if ($logged_user && $user_currencyCode==$trip_currency["code"]){
+if ($logged_user && $user_currencyCode == $trip_currency["code"]) {
 	$user_balance = mysqlFetch(mysqlQuery("SELECT SUM(amount) AS total FROM users_balance WHERE currency='$user_currencyCode' AND user_id='" . $logged_user["id"] . "'"))["total"];
-	if ($user_balance > 0){
+	if ($user_balance > 0) {
 		$subtract_from_balance = min($user_balance, $trip_price);
 	}
 }
@@ -173,7 +174,9 @@ $trip_price = $trip_price - $subtract_from_balance;
 
 //Section Information
 $section_information = mysqlFetch(mysqlQuery("SELECT * FROM " . $suffix . "website_pages WHERE page='" . basename($_SERVER["SCRIPT_FILENAME"], ".php") . "'"));
-if ($section_information["hidden"]){ brokenLink(); }
+if ($section_information["hidden"]) {
+	brokenLink();
+}
 $section_title = $section_information["title"];
 $section_description = $section_information["description"];
 $section_header_image = ($section_information["header_image"] ? "uploads/pages/" . $section_information["header_image"] : null);
@@ -184,100 +187,104 @@ $section_layout = $section_information["layout"];
 
 //Breadcrumbs
 $breadcrumbs = array();
-array_push($breadcrumbs,"<li><a href='.'>" . readLanguage('general','home') . "</a></li>");
-array_push($breadcrumbs,"<li>" . $section_information["title"] . "</li>");
+array_push($breadcrumbs, "<li><a href='.'>" . readLanguage('general', 'home') . "</a></li>");
+array_push($breadcrumbs, "<li>" . $section_information["title"] . "</li>");
 
 include "system/header.php";
 include "website/section_header.php"; ?>
 
-<?=$message?>
+<?= $message ?>
 
-<script src="plugins/moment.min.js?v=<?=$system_settings["system_version"]?>"></script>
-<script src="plugins/caleran.min.js?v=<?=$system_settings["system_version"]?>"></script>
-<link href="plugins/caleran.min.css?v=<?=$system_settings["system_version"]?>" rel="stylesheet">
+<script src="plugins/moment.min.js?v=<?= $system_settings["system_version"] ?>"></script>
+<script src="plugins/caleran.min.js?v=<?= $system_settings["system_version"] ?>"></script>
+<link href="plugins/caleran.min.css?v=<?= $system_settings["system_version"] ?>" rel="stylesheet">
 
 <!-- Flight Details Modal -->
-<div class="modal fade modal_flight_details"><div class=modal-dialog><div class=modal-content>
-	<div class=modal-header>
-		<button type=button class=close data-dismiss=modal><span>&times;</span></button>
-		<h4 class=modal-title><?=readlanguage('booking','flight_details')?></h4>
+<div class="modal fade modal_flight_details">
+	<div class=modal-dialog>
+		<div class=modal-content>
+			<div class=modal-header>
+				<button type=button class=close data-dismiss=modal><span>&times;</span></button>
+				<h4 class=modal-title><?= readlanguage('booking', 'flight_details') ?></h4>
+			</div>
+			<div class=modal-body></div>
+		</div>
 	</div>
-	<div class=modal-body></div>
-</div></div></div>
+</div>
 
 <!-- Flight Details Content -->
-<? foreach ($itinerary["trips"] AS $index=>$trip){ ?>		
-	<div trip-index=<?=$index?>><?=renderFlightDetails($trip["flights"], $trip["penalties"])?></div>
+<? foreach ($itinerary["trips"] as $index => $trip) { ?>
+	<div trip-index=<?= $index ?>><?= renderFlightDetails($trip["flights"], $trip["penalties"]) ?></div>
 <? } ?>
 
 <!-- Page Content -->
 <div class="row grid-container-15">
 
-<!-- Passengers & booking -->
-<div class="col-md-14 grid-item">
-	<?
-	//Register passengers
-	if ($booking_session["status"]==0){
-		include "_inl_booking_passengers.php";
-	
-	//Payment
-	} else if ($booking_session["status"]==1){
-		$payment_redirect = $base_url . "checkout/" . $booking_session["session"] . "/";
-		$payment_amount = $trip_price;
-		$payment_currency = $itinerary["currency"];
-		include "_inl_booking_payment.php";
-	}
-	?>
-</div>
+	<!-- Passengers & booking -->
+	<div class="col-md-14 grid-item">
+		<?
+		//Register passengers
+		if ($booking_session["status"] == 0) {
+			include "_inl_booking_passengers.php";
 
-<!-- Side Column -->
-<div class="col-md-6 grid-item">
-	<div class=page_container>
-		<div class=page_subtitle><?=readlanguage('booking','flight_summary')?></div>
-		<? foreach ($itinerary["trips"] AS $index=>$trip){ ?>
-			<div class=trip_summary>
-				<i class="fas fa-plane"></i>&nbsp;&nbsp;&nbsp;&nbsp;
-				<div>
-					<b><?=$trip["from"]["airport"]["ar_short_name"]?> - <?=$trip["to"]["airport"]["ar_short_name"]?></b>
-					<span><?=dateLanguage("l, d M Y", $trip["date"])?></span>
-					<small><?=dateLanguage("H:i A", $trip["flights"][0]["takeoff"]["time"])?></small>
-				</div>
-				<a class="btn btn-default btn-sm" onclick="$('.modal_flight_details').find('.modal-body').html($('[trip-index=<?=$index?>]').html()); $('.modal_flight_details').modal('show');"><?=readlanguage('common','details')?></a>
-			</div>
-		<? } ?>
-		
-		<div class="page_subtitle margin-top-20"><?=readlanguage('reservation','price_details')?></div>
-		<table class=trip_price_summary>
-			<tr>
-				<th>التذكرة</th>
-				<th class=center-large><?=readlanguage('common','price')?></th>
-				<!--<th>الضرائب</th>-->
-				<!--<th>العمولة</th>-->
-				<th class=center-large><?=readlanguage('common','number')?></th>
-				<th><?=readlanguage('common','amount')?></th>
-			</tr>
-			<? foreach ($itinerary["trips"][0]["pricing"] AS $type=>$pricing){ ?>
-			<tr>
-				<td><?=getDictionary($type)?></td>
-				<td class=center-large><?=number_format($pricing["units"] + $pricing["commission"], 2)?></td>
-				<!--<td class=center-large><?=$pricing["taxes"]?></td>-->
-				<!--<td class=center-large><?=$pricing["commission"]?></td>-->
-				<td class=center-large><?=$pricing["count"]?></td>
-				<td><b><?=number_format($pricing["total"], 2)?></b> <small><?=$itinerary["currency"]?></small></td>
-			</tr>
-			<? } ?>
-		</table>
-		
-		<? if ($subtract_from_balance){ ?>
-			<div class="page_subtitle margin-top-20"><?=readLanguage('booking','subtract_from_balance')?></div>
-			<div class=blance_price><b><?=number_format($subtract_from_balance, 2)?></b> <small><?=$trip_currency["ar_name"]?></small></div>
-			<span><?=readLanguage('booking','balance_after_subtract')?> <b><?=number_format($user_balance - $subtract_from_balance, 2)?></b> <small><?=$trip_currency["ar_name"]?></small></span>
-		<? } ?>
-		
-		<div class="page_subtitle margin-top-20"><?=readLanguage('common','total')?></div>
-		<div class=trip_price><b style="color:<?=($trip_price ? "red" : "green")?>"><?=number_format($trip_price, 2)?></b> <small><?=$trip_currency["ar_name"]?></small></div>
+			//Payment
+		} else if ($booking_session["status"] == 1) {
+			$payment_redirect = $base_url . "checkout/" . $booking_session["session"] . "/";
+			$payment_amount = $trip_price;
+			$payment_currency = $itinerary["currency"];
+			include "_inl_booking_payment.php";
+		}
+		?>
 	</div>
-</div>
+
+	<!-- Side Column -->
+	<div class="col-md-6 grid-item">
+		<div class=page_container>
+			<div class=page_subtitle><?= readlanguage('booking', 'flight_summary') ?></div>
+			<? foreach ($itinerary["trips"] as $index => $trip) { ?>
+				<div class=trip_summary>
+					<i class="fas fa-plane"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+					<div>
+						<b><?= $trip["from"]["airport"]["ar_short_name"] ?> - <?= $trip["to"]["airport"]["ar_short_name"] ?></b>
+						<span><?= dateLanguage("l, d M Y", $trip["date"]) ?></span>
+						<small><?= dateLanguage("H:i A", $trip["flights"][0]["takeoff"]["time"]) ?></small>
+					</div>
+					<a class="btn btn-default btn-sm" onclick="$('.modal_flight_details').find('.modal-body').html($('[trip-index=<?= $index ?>]').html()); $('.modal_flight_details').modal('show');"><?= readlanguage('common', 'details') ?></a>
+				</div>
+			<? } ?>
+
+			<div class="page_subtitle margin-top-20"><?= readlanguage('reservation', 'price_details') ?></div>
+			<table class=trip_price_summary>
+				<tr>
+					<th>التذكرة</th>
+					<th class=center-large><?= readlanguage('common', 'price') ?></th>
+					<!--<th>الضرائب</th>-->
+					<!--<th>العمولة</th>-->
+					<th class=center-large><?= readlanguage('common', 'number') ?></th>
+					<th><?= readlanguage('common', 'amount') ?></th>
+				</tr>
+				<? foreach ($itinerary["trips"][0]["pricing"] as $type => $pricing) { ?>
+					<tr>
+						<td><?= getDictionary($type) ?></td>
+						<td class=center-large><?= number_format($pricing["units"] + $pricing["commission"], 2) ?></td>
+						<!--<td class=center-large><?= $pricing["taxes"] ?></td>-->
+						<!--<td class=center-large><?= $pricing["commission"] ?></td>-->
+						<td class=center-large><?= $pricing["count"] ?></td>
+						<td><b><?= number_format($pricing["total"], 2) ?></b> <small><?= $itinerary["currency"] ?></small></td>
+					</tr>
+				<? } ?>
+			</table>
+
+			<? if ($subtract_from_balance) { ?>
+				<div class="page_subtitle margin-top-20"><?= readLanguage('booking', 'subtract_from_balance') ?></div>
+				<div class=blance_price><b><?= number_format($subtract_from_balance, 2) ?></b> <small><?= $trip_currency["ar_name"] ?></small></div>
+				<span><?= readLanguage('booking', 'balance_after_subtract') ?> <b><?= number_format($user_balance - $subtract_from_balance, 2) ?></b> <small><?= $trip_currency["ar_name"] ?></small></span>
+			<? } ?>
+
+			<div class="page_subtitle margin-top-20"><?= readLanguage('common', 'total') ?></div>
+			<div class=trip_price><b style="color:<?= ($trip_price ? "red" : "green") ?>"><?= number_format($trip_price, 2) ?></b> <small><?= $trip_currency["ar_name"] ?></small></div>
+		</div>
+	</div>
 
 </div>
 
